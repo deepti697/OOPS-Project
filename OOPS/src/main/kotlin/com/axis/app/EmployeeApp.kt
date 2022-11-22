3f23a0ce9a1f7a0732086a056f382b6abc2eee0b
deepti697 <116783856+deepti697@users.noreply.github.com>
2022-11-22 15:41:05 +0530

    Add files via upload

---
+++ OOPS/src/main/kotlin/com/axis/app/EmployeeApp.kt
@@ -0,0 +1,14 @@
+package com.axis.app
+import com.axis.model.Employee
+import com.axis.service.EmployeeService
+
+fun main(args: Array<String>) {
+    println("=====Employee App=====")
+    var employee=Employee(1,"Deepti",60000)
+    var employeeService=EmployeeService()
+
+    employeeService.displayEmployeeDetails(employee)
+
+    println("Per Day Salary : ${employeeService.perDaySalray(employee)}")
+    println("Increased Salary by 10% : ${employeeService.increaseSalByTenPercent(employee)}")
+}
